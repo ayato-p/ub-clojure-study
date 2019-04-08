02d34d055e5b667aa3c5a4326d424fc612f03a48
ayato_p <ayato.skycrawlers@gmail.com>
2019-04-08 09:17:17 +0900

    第2回終了時の状態

--- rss-parser/src/app/core.clj
+++ rss-parser/src/app/core.clj
@@ -2,39 +2,42 @@
   (:gen-class)
   (:require [clj-http.client :as cli]
             [clojure.data.csv :as csv]
+            [clojure.java.io :as io]
             [clojure.xml :as xml]
-            [clojure.java.io :as io])
+            [clojure.string :as str])
   (:import java.io.ByteArrayInputStream))
 
 ;; - http://b.hatena.ne.jp/hotentry/{category}.rss
 ;; - all, general, social, economics, life, knowledge, it, fun, entertainment, game
 
-(defn str->input-stream [s]
+(defn str->input-stream [^String s]
   (ByteArrayInputStream. (.getBytes s)))
 
 (defn get-hotentry [category]
   (cli/get (str "http://b.hatena.ne.jp/hotentry/" category ".rss")))
 
-(def parsed (xml/parse (str->input-stream (:body (get-hotentry "it")))))
-
-(def filterd (filter #(= :item (:tag %)) (get-in parsed [:content])))
-
-(def item (first filterd))
-
 (defn extract [item]
   (reduce-kv #(assoc %1 %2 (get-in %3 [:content 0]))
              {}
-             (select-keys (zipmap (map :tag (:content item))
-                                  (:content item))
+             (select-keys (zipmap (:content item)
+                                  (map :tag (:content item)))
                           [:title :link :dc:date])))
 
-(with-open [w (io/writer (io/file "/tmp/it.csv"))]
-  (csv/write-csv w (map (juxt :title :link :dc:date)
-                        (take 2 (map extract filterd)))
-                 :quote? (constantly true)))
+(defn write-csv [filename items]
+  (with-open [w (io/writer (io/file (str "/tmp/hotentry/" filename)))]
+    (csv/write-csv w items :quote? (constantly true))))
 
-
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn -main [& args]
+  (doseq [category ["all" "general" "social" "economics"
+                    "life" "knowledge" "it" "fun"
+                    "entertainment" "game"]
+          :let [filename (str category ".csv")
+                items (->> (get-hotentry category)
+                           :body
+                           str->input-stream
+                           xml/parse
+                           :content
+                           (filter #(= :item (:tag %)))
+                           (map extract)
+                           (map (juxt :title :link :dc:date)))]]
+    (write-csv filename items)))
